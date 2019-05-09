9e49f3dc917cc0a701997303b12a69e044ca6932
Tobias Norton <tnorton20@yahoo.com>
2019-05-08 20:44:23 -0400

    Adds background image and nav bar to Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render() {
-    return <div />
+    return (
+      <div className="home">
+        <nav className="nav-bar">
+          <div className="nav-right">
+            <p>About</p>
+            <p>Contact</p>
+            <p>Other Good Stuff</p>
+            <p>Blog</p>
+            <button>Set Appointment</button>
+          </div>
+        </nav>
+        <h3>Being healthy doesn't have to be difficult.</h3>
+        <button>Set Appointment</button>
+      </div>
+    )
   }
 }
 
