732476ab568abf601a7ecd061dff5440fae7b433
Tobias Norton <tnorton20@yahoo.com>
2019-05-09 22:10:13 -0400

    Adds Message section and button CSS to Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,19 +3,34 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <nav className="nav-bar">
-          <div className="nav-right">
-            <p>About</p>
-            <p>Contact</p>
-            <p>Other Good Stuff</p>
-            <p>Blog</p>
-            <button className="nav-appt">Set Appointment</button>
-          </div>
-        </nav>
-        <h3>Being healthy doesn't have to be difficult.</h3>
-        <button className="body-appt">Set Appointment</button>
-      </div>
+      <>
+        <div className="home">
+          <nav className="nav-bar">
+            <div className="nav-right">
+              <p>About</p>
+              <p>Contact</p>
+              <p>Blog</p>
+              <p>Other Good Stuff</p>
+              <button className="nav-appt">Set Appointment</button>
+            </div>
+          </nav>
+          <section className="main">
+            <h3>Being healthy doesn't have to be hard.</h3>
+            <button className="body-appt">Set Appointment</button>
+          </section>
+        </div>
+
+        <section className="message">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+        </section>
+      </>
     )
   }
 }
