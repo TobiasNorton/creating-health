ea69e2458853ed0633d0507cfa7906e7fd69a7a0
Tobias Norton <tnorton20@yahoo.com>
2019-05-13 17:46:34 -0400

    Creates routes for nav bar items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import RelationshipRepair from './Components/RelationshipRepair'
 import SevenLessons from './Components/SevenLessons'
 import CorporateWellness from './Components/CorporateWellness'
 import Membership from './Components/Membership'
+import About from './Components/About'
+import Blog from './Components/Blog'
+import Contact from './Components/Contact'
+import Other from './Components/Other'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -17,6 +21,10 @@ function App() {
         <Route exact path="/relationship-repair" component={RelationshipRepair} />
         <Route exact path="/corporate-wellness" component={CorporateWellness} />
         <Route exact path="/membership" component={Membership} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/other-good-stuff" component={Other} />
       </div>
     </Router>
   )
