55a27d3c3a91cbb53c4998fcab672d3f3b63a78d
Tobias Norton <tnorton20@yahoo.com>
2019-05-16 19:29:18 -0400

    Adds and formats content for CorporateWellness component

--- src/Components/CorporateWellness.js
+++ src/Components/CorporateWellness.js
@@ -2,7 +2,107 @@ import React, { Component } from 'react'
 
 class CorporateWellness extends Component {
   render() {
-    return <div />
+    return (
+      <div className="home">
+        <nav className="nav-bar">
+          <div className="nav-right">
+            <p>About</p>
+            <p>Contact</p>
+            <p>Blog</p>
+            <p>Other Good Stuff</p>
+            <button className="nav-appt">Set Appointment</button>
+          </div>
+        </nav>
+        <section className="main">
+          <h3>The Real Cost of Employee Disengagement</h3>
+          <p className="corp-wellness">
+            Do you know the level of employee disengagement at your business? According to a recent
+            Gallup poll, only 33% of workers in the United States are engaged in their jobs, which
+            means the remaining 51% of employees are disengaged and 16% are actively disengaged.
+          </p>
+          <h4>What is employee engagement?</h4>{' '}
+          <p>
+            Gallup defines engaged employees as those who are involved in, enthusiastic about and
+            committed to their work and workplace. Gallup categorizes workers as engaged, not
+            engaged or actively disengaged based on their responses to key workplace elements.
+            <ul>
+              <li>
+                Engaged: Employees are highly involved in and enthusiastic about their work and
+                workplace. They are psychological “owners,” drive performance and innovation, and
+                move the organization forward.
+              </li>
+              <li>
+                Not engaged: Employees are psychologically unattached to their work and company.
+                Because their engagement needs are not being fully met, they’re putting time — but
+                not energy or passion — into their work.
+              </li>
+              <li>
+                Actively disengaged: Employees aren’t just unhappy at work — they are resentful that
+                their needs aren’t being met and are acting out their unhappiness. Every day, these
+                workers potentially undermine what their engaged coworkers accomplish.
+              </li>
+            </ul>
+            <h4>What does that mean for you?</h4>
+            <p>Disengaged employees are either:</p>
+            <ul>
+              <li>just co-existing in your workplace</li>
+              <li>saying negative things about your company, or</li>
+              <li>
+                lacking any involvement with their teams or initiatives to keep pushing the company
+                forward.
+              </li>
+            </ul>
+            <h4>Bottom Line</h4>
+            <p>
+              Disengaged employees are costing you money and affecting your profits. The hard-nosed
+              reality is that if you do nothing to help employee disengagement, you are wasting
+              money each year. Each disengaged employee costs you 34% of their salary because of
+              lost productivity, missed shifts, chronic tardiness, and disrupting others by
+              spreading negativity throughout the company. Actively disengaged employees are
+              miserable in the workplace and destroy the positive benefits engaged employees work so
+              hard to build for the company. <br />
+              <br />
+              Businesses often think employee engagement is about satisfaction and happiness, but it
+              goes much deeper. Employee engagement involves how much workers want to be engaged in
+              the business, be proactive about their schedule, and be positive about where the
+              company is going and how they treat their coworkers.
+            </p>
+            <h4>A Key to Better Engagement</h4>
+            <p>
+              Disengaged employees lose the motivation to give their best effort to help your
+              business succeed. Actively disengaged employees cost the U.S. $483 billion to $605
+              billion each year in lost productivity. A high price to pay for disengagement. By
+              helping employees with Relationship Repair you’ll start seeing higher productivity,
+              lower levels of sick leave use, less work time missed due to workplace injury or
+              illness, lower levels of attrition, and higher levels of employee satisfaction.
+              According to Gallup, improving employee engagement strongly impacts the overall
+              well-being of your business. Employee engagement was found to have the following
+              effects:
+            </p>
+            <ul>
+              <li>17% higher productivity</li>
+              <li>21% higher profitability</li>
+              <li>10% higher customer satisfaction</li>
+              <li>41% lower absenteeism</li>
+              <li>24% less turnover (in high-turnover organizations)</li>
+              <li>59% less turnover (in low-turnover organizations)</li>
+              <li>70% fewer safety incidents </li>
+              <li>40% fewer defects and quality issues</li>
+            </ul>
+            The truth comes down to this: if you ignore employee disengagement, employees will opt
+            out at work. Providing Relationship Repair will not only help your employees with family
+            conflict, it will impact workplace conflict amounting to higher engagement along with
+            higher productivity within your business, which means happier employees and higher
+            profits.
+            <br />
+            <br /> Schedule an appointment here. Work place engagement has improved through
+            Relationship Repair resulting in organizations across nearly every industry successfully
+            maximize millions of dollars to their organizations.
+          </p>
+        </section>
+        <section className="message" />
+      </div>
+    )
   }
 }
 
