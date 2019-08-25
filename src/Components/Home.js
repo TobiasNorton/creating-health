a035e873c3c91604d4624ea64f5def46a1cbb367
Tobias Norton <tnorton20@yahoo.com>
2019-08-25 16:33:13 -0400

    Adds H1 header

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
         <div className="home">
           <NavBar />
           <section className="main">
-            {/* <h3>Find peace in your relationships.</h3> */}
+            <h1>Creating Health and Healing</h1>
             {/* <button className="body-appt">Set Appointment</button> */}
             <h3 className="starter-kit">Take a moment to rate your relationships.</h3>
             <Link to="/rate-your-relationships">
